4cf77e5e56dc638a275ef8788dc1b05fe1e5b602
Nikolai Malygin <n.p.malygin@gmail.com>
2023-11-02 12:29:35 +0300

    added new test

--- src/test/java/guru/qa/tests/SearchTests.java
+++ src/test/java/guru/qa/tests/SearchTests.java
@@ -3,9 +3,12 @@ package guru.qa.tests;
 import com.codeborne.selenide.CollectionCondition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.*;
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.appium.java_client.AppiumBy.id;
 
 public class SearchTests extends TestBase {
 
@@ -14,5 +17,17 @@ public class SearchTests extends TestBase {
         $(accessibilityId("Search Wikipedia")).click();
         $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(CollectionCondition.sizeGreaterThan(0));
+    }
+
+    @Test
+    void openArticle() {
+        $(accessibilityId("Search Wikipedia")).click();
+        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("France");
+        $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(CollectionCondition.sizeGreaterThan(0));
+        $$(id("org.wikipedia.alpha:id/page_list_item_title")).first().click();
+        $(id("org.wikipedia.alpha:id/view_wiki_error_icon")).should(exist);
+        $(id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldHave(text("An error occurred"));
+        $(id("org.wikipedia.alpha:id/view_wiki_error_button")).should(exist);
+        $(id("org.wikipedia.alpha:id/view_wiki_error_button")).shouldHave(text("GO BACK"));
     }
 }
