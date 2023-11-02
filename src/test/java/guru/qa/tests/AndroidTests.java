7d51d8f7f89e56a57a364ecf330c7819c2eee15a
Nikolai Malygin <n.p.malygin@gmail.com>
2023-11-02 15:16:08 +0300

    added allure steps

--- src/test/java/guru/qa/tests/AndroidTests.java
+++ src/test/java/guru/qa/tests/AndroidTests.java
@@ -1,6 +1,7 @@
 package guru.qa.tests;
 
 import com.codeborne.selenide.CollectionCondition;
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -10,26 +11,38 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
 
 @Tag("android")
 public class AndroidTests extends TestBase {
 
     @Test
+    @Description("Проверка непустой поисковой выдачи")
     void successfulSearchTest() {
-        $(accessibilityId("Search Wikipedia")).click();
-        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
-        $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(CollectionCondition.sizeGreaterThan(0));
+        step("Поиск через строку поиска", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+        step("Проверка непустой выдачи", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(CollectionCondition.sizeGreaterThan(0)));
     }
 
     @Test
+    @Description("Проверка открытия статьи")
     void openArticle() {
-        $(accessibilityId("Search Wikipedia")).click();
-        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("France");
-        $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(CollectionCondition.sizeGreaterThan(0));
-        $$(id("org.wikipedia.alpha:id/page_list_item_title")).first().click();
-        $(id("org.wikipedia.alpha:id/view_wiki_error_icon")).should(exist);
-        $(id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldHave(text("An error occurred"));
-        $(id("org.wikipedia.alpha:id/view_wiki_error_button")).should(exist);
-        $(id("org.wikipedia.alpha:id/view_wiki_error_button")).shouldHave(text("GO BACK"));
+        step("Поиск через строку поиска", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("France");
+        });
+        step("Проверка непустой выдачи", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(CollectionCondition.sizeGreaterThan(0)));
+        step("Открытие первого элемента из списка", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title")).first().click());
+        step("Проверка элементов на странице с ошибкой", () -> {
+            $(id("org.wikipedia.alpha:id/view_wiki_error_icon")).should(exist);
+            $(id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldHave(text("An error occurred"));
+            $(id("org.wikipedia.alpha:id/view_wiki_error_button")).should(exist);
+            $(id("org.wikipedia.alpha:id/view_wiki_error_button")).shouldHave(text("GO BACK"));
+        });
     }
 }
--- src/test/java/guru/qa/tests/IOSTests.java
+++ src/test/java/guru/qa/tests/IOSTests.java
@@ -2,21 +2,26 @@ package guru.qa.tests;
 
 import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
 
 @Tag("ios")
-public class IOSTests extends TestBase{
+public class IOSTests extends TestBase {
 
     @Test
-    void somethingTest() {
-        $(AppiumBy.className("XCUIElementTypeStaticText")).$(AppiumBy.name("UI Elements")).should(Condition.exist);
-        $(AppiumBy.name("Text Button")).shouldBe(Condition.visible);
-        $(AppiumBy.name("Text")).shouldBe(Condition.visible);
-        $(AppiumBy.name("Tab Bar")).should(Condition.exist);
-        $(AppiumBy.name("Web View")).should(Condition.exist);
-        $(AppiumBy.name("Local Testing")).should(Condition.exist);
+    @Description("Проверка отображения элементов")
+    void uiElementsTest() {
+        step("Проверка отображения элементов на главной", () -> {
+            $(AppiumBy.className("XCUIElementTypeStaticText")).$(AppiumBy.name("UI Elements")).should(Condition.exist);
+            $(AppiumBy.name("Text Button")).shouldBe(Condition.visible);
+            $(AppiumBy.name("Text")).shouldBe(Condition.visible);
+            $(AppiumBy.name("Tab Bar")).should(Condition.exist);
+            $(AppiumBy.name("Web View")).should(Condition.exist);
+            $(AppiumBy.name("Local Testing")).should(Condition.exist);
+        });
     }
 }
